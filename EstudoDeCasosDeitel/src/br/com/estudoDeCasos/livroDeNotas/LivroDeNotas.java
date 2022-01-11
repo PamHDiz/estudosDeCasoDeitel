67f6364139a07cb55bbbb53aeec63845aac41c02
PamHDiz <pamhdiz@gmail.com>
2022-01-11 16:17:57 -0300

    Estudo de Casos Livro de Notas

--- EstudoDeCasosDeitel/src/br/com/estudoDeCasos/livroDeNotas/LivroDeNotas.java
+++ EstudoDeCasosDeitel/src/br/com/estudoDeCasos/livroDeNotas/LivroDeNotas.java
@@ -2,4 +2,21 @@ package br.com.estudoDeCasos.livroDeNotas;
 
 public class LivroDeNotas {
 	
+	private String nomeDoCurso; //nome do curso que representa
+	private int[] notas; //array de notas dos alunos
+	
+	public LivroDeNotas(String nomeDoCurso, int[] notas) {
+		this.nomeDoCurso = nomeDoCurso;
+		this.notas = notas;
+	}
+
+	public String getNomeDoCurso() {
+		return nomeDoCurso;
+	}
+	
+	//realiza várias operações com as notas
+	public void processadorDeNotas() {
+		
+	}
+	
 }
